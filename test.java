1c577a90f55bcc085b0d8290e5d205eaa856b540
hanrenbo <53374160+hanrenbo@users.noreply.github.com>
2019-07-27 23:04:15 +0800

    上传文件
    第一次使用GitHub 上传文件

---
+++ test.java
@@ -0,0 +1,12 @@
+public class test{
+
+	public static String upcase(String s){
+		return s.toUpcase();
+	}
+	
+	public static void main(String[] args){
+	
+		String q = "hello";
+		System.out.println(q);
+	}
+}
